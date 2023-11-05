38ce2713063456235a508d51c0acbe3b8dd85a44
David Reid <134351711+D-a-v-i-d-Reid@users.noreply.github.com>
2023-11-05 17:08:00 -0500

    Initial Commit

---
+++ Classes/Buyer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Quarter_Horse_Online_Auction.Classes
+{
+    public class Buyer : User
+    {
+        public int BuyerId { get; set; }
+
+    }
+}
---
+++ Classes/User.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Quarter_Horse_Online_Auction.Classes
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        private string? _userEmail;
+
+        public string? UserEmail
+        {
+            get => _userEmail;
+            set
+            {
+                if (IsValidEmail(value ))
+                {
+                    _userEmail = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid email format");
+                }
+
+
+            }
+
+
+
+        }
+        private bool IsValidEmail(string? email)
+        {
+            if (email == null)
+            {
+                return false;
+            }
+            string emailPattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+    }
+}
